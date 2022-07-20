ce721967053b6a50f83dd894d6c14664b496028b
chou3934 <choumelanie12@gmail.com>
2022-07-21 00:29:57 +0800

    商品功能-實作查詢商品列表（基礎實作）

--- src/main/java/com/melanie/springbootmall/dao/ProductDao.java
+++ src/main/java/com/melanie/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.melanie.springbootmall.dao;
 import com.melanie.springbootmall.dto.ProductRequest;
 import com.melanie.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
