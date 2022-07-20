df7129f5a6d646ad0a17fc9fefc84e541fa5d43d
chou3934 <choumelanie12@gmail.com>
2022-07-20 23:21:33 +0800

    商品功能-實作新增商品

--- src/main/java/com/melanie/springbootmall/dao/ProductDao.java
+++ src/main/java/com/melanie/springbootmall/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.melanie.springbootmall.dao;
 
+import com.melanie.springbootmall.dto.ProductRequest;
 import com.melanie.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/melanie/springbootmall/service/ProductService.java
+++ src/main/java/com/melanie/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.melanie.springbootmall.service;
 
+import com.melanie.springbootmall.dto.ProductRequest;
 import com.melanie.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
