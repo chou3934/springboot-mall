34ff7eb16b2355b1170b15685660435dda046d28
chou3934 <choumelanie12@gmail.com>
2022-07-26 17:50:03 +0800

    商品功能-實作查詢商品列表（分頁＋總比數）

--- src/main/java/com/melanie/springbootmall/controller/ProductController.java
+++ src/main/java/com/melanie/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.melanie.springbootmall.dto.ProductQueryParams;
 import com.melanie.springbootmall.dto.ProductRequest;
 import com.melanie.springbootmall.model.Product;
 import com.melanie.springbootmall.service.ProductService;
+import com.melanie.springbootmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProduct(
+    public ResponseEntity<Page<Product>> getProduct(
             //查詢Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -44,9 +45,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        //取得product list
         List<Product> productList=productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        //取得product總數
+        Integer total=productService.countProduct(productQueryParams);
+
+        //分頁
+        Page<Product> page=new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
     @GetMapping("/products/{productId}")
