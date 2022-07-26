c8ee88d45302f810da0d10d2dec903601222a782
chou3934 <choumelanie12@gmail.com>
2022-07-26 17:04:10 +0800

    商品功能-實作查詢商品列表（分頁）

--- src/main/java/com/melanie/springbootmall/controller/ProductController.java
+++ src/main/java/com/melanie/springbootmall/controller/ProductController.java
@@ -12,8 +12,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
@@ -27,13 +30,19 @@ public class ProductController {
             @RequestParam(required = false) String search,
             //排序Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+            //分頁
+            @RequestParam(defaultValue = "5")@Max(1000)@Min(0) Integer limit,
+            @RequestParam(defaultValue = "0")@Min(0) Integer offset
+
     ){
         ProductQueryParams productQueryParams=new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList=productService.getProducts(productQueryParams);
 
