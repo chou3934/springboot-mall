90999765dd1f18266e67c1463ff40169ca22d069
chou3934 <choumelanie12@gmail.com>
2022-07-28 16:16:14 +0800

    帳號功能-ㄐ檢查註冊的email、隱藏返回的密碼

--- src/main/java/com/melanie/springbootmall/service/implement/UserServiceImpl.java
+++ src/main/java/com/melanie/springbootmall/service/implement/UserServiceImpl.java
@@ -4,22 +4,39 @@ import com.melanie.springbootmall.dao.implement.UserDaoImpl;
 import com.melanie.springbootmall.dto.UserRegisterRequest;
 import com.melanie.springbootmall.model.User;
 import com.melanie.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final  static Logger log= LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDaoImpl userDao;
 
     @Override
     public User getUserById(Integer userId) {
+
         return userDao.getUserById(userId);
     }
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        //檢查email
+        User user= userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user!=null){
+            log.warn("該 email{} 已註冊",userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //創造
         return userDao.createUser(userRegisterRequest);
     }
 }
